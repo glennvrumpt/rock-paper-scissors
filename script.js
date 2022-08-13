bf138a1a0a517c16be53a4785a835a9017e5da84
Glenn van Rumpt <glennvrumpt@outlook.com>
2022-08-13 09:01:43 +0200

    Add ui

--- script.js
+++ script.js
@@ -1,48 +1,94 @@
 let playerScore = 0;
 let computerScore = 0;
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(function (e) {
+  e.addEventListener('click', function () {
+    if (e === buttons[0]) {
+      playerSelection = 'Rock';
+    } else if (e === buttons[1]) {
+      playerSelection = 'Paper';
+    } else {
+      playerSelection = 'Scissors';
+    }
+    game();
+  });
+});
+
+const disableButton = () => {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
+  }
+};
+
+function game() {
+  const computerSelection = getComputerChoice();
+  let gameMessage = playRound(playerSelection, computerSelection);
+  updateScore(gameMessage);
+  displayChoice(playerSelection, computerSelection);
+  let winner = document.querySelector('.winner');
+  if (playerScore == 5) {
+    winner.innerHTML = 'You win the game!';
+    disableButton();
+  } else if (computerScore == 5) {
+    winner.innerHTML = 'You lose the game!';
+    disableButton();
+  }
+}
 
 function getComputerChoice() {
-  const choices = ["Rock", "Paper", "Scissors"];
+  const choices = ['Rock', 'Paper', 'Scissors'];
   let random = Math.floor(Math.random() * choices.length);
   return choices[random];
 }
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
 function playRound(playerSelection, computerSelection) {
-  let gameState = "";
-  let playerChoice = capitalizeFirstLetter(playerSelection.toLowerCase());
-  if (playerChoice === computerSelection) {
-    gameState = "Tied";
+  let gameState = '';
+  if (playerSelection === computerSelection) {
+    gameState = 'Tied!';
   } else if (
-    (playerChoice == "Rock" && computerSelection == "Scissors") ||
-    (playerChoice == "Paper" && computerSelection == "Rock") ||
-    (playerChoice == "Scissors" && computerSelection == "Paper")
+    (playerSelection == 'Rock' && computerSelection == 'Scissors') ||
+    (playerSelection == 'Paper' && computerSelection == 'Rock') ||
+    (playerSelection == 'Scissors' && computerSelection == 'Paper')
   ) {
-    gameState = `You Win! ${playerChoice} beats ${computerSelection}`;
+    gameState = `You win the round! ${playerSelection} beats ${computerSelection}`;
     playerScore++;
   } else {
-    gameState = `You Lose! ${computerSelection} beats ${playerChoice}`;
+    gameState = `You lose the round! ${computerSelection} beats ${playerSelection}`;
     computerScore++;
   }
   return gameState;
 }
 
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    const computerSelection = getComputerChoice();
-    const playerSelection = prompt("Choose Rock, Paper or Scissors");
-    playRound(playerSelection, computerSelection);
-  }
-  if (playerScore > computerScore) {
-    return "You Win!";
-  } else if (playerScore < computerScore) {
-    return "You Lose!";
-  } else {
-    return "Tie!";
-  }
+function updateScore(gameMessage) {
+  document.querySelector('.message').innerHTML = gameMessage;
+  document.querySelector('.player').innerHTML = playerScore;
+  document.querySelector('.computer').innerHTML = computerScore;
 }
 
-console.log(game());
+function displayChoice(playerSelection, computerSelection) {
+  const p = document.querySelector('.player-selection');
+  const c = document.querySelector('.computer-selection');
+  switch (playerSelection) {
+    case 'Rock':
+      p.innerHTML = '<img src="images/477918-200.png" />';
+      break;
+    case 'Paper':
+      p.innerHTML = '<img src="images/477922-200.png" />';
+      break;
+    case 'Scissors':
+      p.innerHTML = '<img src="images/477919-200.png" />';
+      break;
+  }
+  switch (computerSelection) {
+    case 'Rock':
+      c.innerHTML = '<img src="images/477918-200.png" />';
+      break;
+    case 'Paper':
+      c.innerHTML = '<img src="images/477922-200.png" />';
+      break;
+    case 'Scissors':
+      c.innerHTML = '<img src="images/477919-200.png" />';
+      break;
+  }
+}
